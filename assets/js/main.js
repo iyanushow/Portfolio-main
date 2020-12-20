fb7f6046afd4eddc362f45bb144b8e28c7b6588f
iyanushow <iyanuofficial@gmail.com>
2020-12-20 02:19:52 +0100

    new portfolio commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,12 +54,7 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+  
 
 
 });
